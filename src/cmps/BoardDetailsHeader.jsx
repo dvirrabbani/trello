28ab5b6886b6e5100d30374e27a08d894bbbd748
Dvir <silentwisdom75@gmail.com>
2024-04-01 22:49:11 +0300

    feat - add BoardDetailsHeader

--- src/cmps/BoardDetailsHeader.jsx
+++ src/cmps/BoardDetailsHeader.jsx
@@ -1,3 +1,16 @@
-export function BoardDetailsHeader({ title }) {
-  return <section className="board-details-header">{title}</section>;
+export function BoardDetailsHeader({ title, members }) {
+  return (
+    <section className="board-details-header">
+      <h3 className="board-details-title fs18">{title}</h3>
+      <ul className="member-list">
+        {members.map((member) => {
+          return (
+            <li key={member._id} className="member-item">
+              <img src={member.imgUrl} alt="member image" />
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
 }
