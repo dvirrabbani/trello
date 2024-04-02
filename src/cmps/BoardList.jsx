ab667704307add2b98722468d44124548db8862b
Dvir <silentwisdom75@gmail.com>
2024-04-02 15:04:51 +0300

    wip - add new board button, implement UI layout

--- src/cmps/BoardList.jsx
+++ src/cmps/BoardList.jsx
@@ -1,11 +1,11 @@
-import { BoardPreview } from "./boardPreview";
-import SvgIcon from "./SvgIcon";
+import { BoardPreview } from "./boardPreview"
+import SvgIcon from "./SvgIcon"
 
 export function BoardList({ boards }) {
   return (
     <ul className="board-list">
       {boards.map((board) => {
-        const { _id, title, style } = board;
+        const { _id, title, style } = board
         return (
           <li className="board-item" key={board._id}>
             <BoardPreview id={_id} title={title} style={style} />
@@ -13,8 +13,13 @@ export function BoardList({ boards }) {
               <SvgIcon iconName={board.isStarred ? "starFill" : "star"} />
             </div>
           </li>
-        );
+        )
       })}
+      <li className="board-item">
+        <button className="btn-secondary">
+          <span>Create new board</span>
+        </button>
+      </li>
     </ul>
-  );
+  )
 }
