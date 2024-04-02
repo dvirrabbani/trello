01e1d7c3e272bb19c2bf1ce4587e96f1cae19bf0
Dvir <silentwisdom75@gmail.com>
2024-04-02 12:24:33 +0300

    Style -  add Profile icon to board list header in workspace page

--- src/pages/Workspace.jsx
+++ src/pages/Workspace.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { loadBoards } from "../store/board.actions.js";
 import { BoardList } from "../cmps/BoardList.jsx";
+import SvgIcon from "../cmps/SvgIcon.jsx";
 
 export function Workspace() {
   const boards = useSelector((storeState) => storeState.boardModule.boards);
@@ -12,7 +13,10 @@ export function Workspace() {
 
   return (
     <div className="workspace-container">
-      <div className="board-list-header">Boards</div>
+      <div className="board-list-header">
+        <SvgIcon iconName={"profile"} size={"md"} />
+        <span>Your Boards</span>
+      </div>
       <BoardList boards={boards} />
     </div>
   );
