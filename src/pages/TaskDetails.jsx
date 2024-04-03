c6d0637c66dae654c3b44783d4dbb291d256faf4
Dvir <silentwisdom75@gmail.com>
2024-04-03 16:05:42 +0300

    wip - task details - create html skeleton

--- src/pages/TaskDetails.jsx
+++ src/pages/TaskDetails.jsx
@@ -11,8 +11,26 @@ export function TaskDetails() {
   }, [])
 
   return (
-    <dialog ref={ref}>
-      <Link to={`/board/${params.boardId}`}>Close</Link>
+    <dialog ref={ref} className="task-details">
+      <div className="task-header">
+        <Link to={`/board/${params.boardId}`}>x</Link>
+        <div className="task-cover">{/*TODO*/}</div>
+        <div className="task-title">{/*TODO*/}</div>
+        <div className="task-sub-title">{/*TODO*/}</div>
+      </div>
+      <div className="main">
+        <div className="main-header">
+          <div className="main-header-card">{/*TODO*/}</div>
+          <div className="main-header-card">{/*TODO*/}</div>
+          <div className="main-header-card">{/*TODO*/}</div>
+        </div>
+        <div className="task-detail-list">{/*TODO*/}</div>
+        <div className="task-checklist">{/*TODO*/}</div>
+        <div className="task-description">{/*TODO*/}</div>
+        <div className="task-attachments">{/*TODO*/}</div>
+        <div className="task-activity">{/*TODO*/}</div>
+      </div>
+      <div className="sidebar-container">{/*TODO*/}</div>
     </dialog>
   )
 }
