8011f84af4517212bd3343509e63628e488a5971
Dvir <silentwisdom75@gmail.com>
2024-04-07 17:50:25 +0300

    style - replace close Icon

--- src/cmps/TaskDetails/TaskDetailsHeader.jsx
+++ src/cmps/TaskDetails/TaskDetailsHeader.jsx
@@ -3,16 +3,18 @@ import SvgIcon from "../SvgIcon"
 
 export function TaskDetailsHeader({ params, task }) {
   return (
-    <div className="task-details-header flex justify-between">
+    <div className="task-details-header">
       <div className="task-header">
         <div className="task-cover">{/*TODO*/}</div>
-        <div className="task-title flex">
+        <div className="task-title">
           <SvgIcon iconName={"taskWindow"} />
           <h2>{task.title}</h2>
         </div>
         <div className="task-sub-title">{/*TODO*/}</div>
       </div>
-      <Link to={`/board/${params.boardId}`}>x</Link>
+      <Link to={`/board/${params.boardId}`}>
+        <SvgIcon iconName={"close"} />
+      </Link>
     </div>
   )
 }
