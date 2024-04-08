cc0bd4d15d2d719c0641c5bbc617b2ea1721d2d7
Dvir <silentwisdom75@gmail.com>
2024-04-08 11:26:29 +0300

    feat - add reset form logic to use form hook

--- src/customHooks/useForm.js
+++ src/customHooks/useForm.js
@@ -1,27 +1,28 @@
 import { useState } from "react"
 
 export const useForm = (initialState) => {
-    const [fields, setFields] = useState(initialState)
+  const [fields, setFields] = useState(initialState)
 
-    function handleChange({ target }) {
-        let { value, name: field, type, checked } = target
-        // value = (type === 'number') ? +value : value
-        switch (type) {
-            case 'number':
-            case 'range':
-                value = +value
-                break;
-            case 'checkbox':
-                value = checked
+  function handleChange({ target }) {
+    let { value, name: field, type, checked } = target
+    // value = (type === 'number') ? +value : value
+    switch (type) {
+      case "number":
+      case "range":
+        value = +value
+        break
+      case "checkbox":
+        value = checked
 
-            default:
-                break;
-        }
-        setFields((prevFields) => ({ ...prevFields, [field]: value }))
+      default:
+        break
     }
+    setFields((prevFields) => ({ ...prevFields, [field]: value }))
+  }
 
+  function resetForm() {
+    setFields(() => initialState)
+  }
 
-    return [fields, setFields, handleChange]
-
-
+  return [fields, setFields, handleChange, resetForm]
 }
