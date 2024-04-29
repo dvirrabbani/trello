4b40cbd40b1548d02cc95fc2409805cbebf4938d
Dvir <rabbanidvir@gmail.com>
2024-04-29 20:51:39 +0300

    wip - task date popover - style UI, add date input, remove due date from task

--- src/cmps/DynamicTaskPopover/TaskDatesPopover.jsx
+++ src/cmps/DynamicTaskPopover/TaskDatesPopover.jsx
@@ -1,16 +1,26 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider"
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs"
 import { DateCalendar } from "@mui/x-date-pickers/DateCalendar"
+import { DateField, TimeField } from "@mui/x-date-pickers"
 import dayjs from "dayjs"
 import { Button } from "../Button"
 
-export function TaskDatesPopover({ task, onUpdateTask, onClose }) {
-  const [dueDate, setDueDate] = useState(dayjs(task?.dueDate?.date || null))
+export function TaskDatesPopover({
+  task,
+  onUpdateTask,
+  onClose,
+  defaultDueDate = dayjs(new Date()),
+}) {
+  const [dueDate, setDueDate] = useState(
+    dayjs(task?.dueDate?.date || defaultDueDate)
+  )
+  const [isDueDateChecked, setIsDueDateChecked] = useState(true)
+  const prevDueDateRef = useRef(dayjs(task?.dueDate?.date || defaultDueDate))
 
   function onSaveTaskDate() {
     const dueDateToSave = {
-      isCompleted: task.dueDate.isCompleted || false,
+      isCompleted: task?.dueDate?.isCompleted || false,
       date: new Date(dueDate.$d).getTime(),
     }
 
@@ -22,13 +32,61 @@ export function TaskDatesPopover({ task, onUpdateTask, onClose }) {
     onClose()
   }
 
+  function onChangeDueDate(value) {
+    setDueDate(value)
+    prevDueDateRef.current = value
+  }
+
+  function onChangeDueDateChecked() {
+    setIsDueDateChecked((prev) => !prev)
+    setDueDate(isDueDateChecked ? null : prevDueDateRef.current)
+  }
+
+  function onRemoveDueDate() {
+    if (task?.dueDate) {
+      onUpdateTask({
+        key: "dueDate",
+        value: null,
+      })
+    }
+    onClose()
+  }
+
   return (
     <div className="task-dates-popover">
       <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <DateCalendar value={dueDate} onChange={(value) => setDueDate(value)} />
+        <DateCalendar value={dueDate} onChange={onChangeDueDate} />
+        <div className="task-due-date">
+          <label>Due date</label>
+          <section className="form-group flex">
+            <input
+              type="checkbox"
+              checked={Boolean(isDueDateChecked)}
+              onChange={onChangeDueDateChecked}
+            />
+
+            <DateField
+              value={dueDate}
+              onChange={onChangeDueDate}
+              InputProps={{
+                disabled: !isDueDateChecked,
+              }}
+            />
+            <TimeField
+              value={dueDate}
+              onChange={onChangeDueDate}
+              InputProps={{
+                disabled: !isDueDateChecked,
+              }}
+            />
+          </section>
+        </div>
       </LocalizationProvider>
       <Button variant="primary" onClick={onSaveTaskDate}>
         Save
+      </Button>
+      <Button variant="group" onClick={onRemoveDueDate}>
+        Remove
       </Button>
     </div>
   )
