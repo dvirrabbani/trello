9e1d8535e438cbc1591d8d22445df40df12cf1cb
2xlyuval <2xlyuva@gmail.com>
2024-05-25 13:36:19 +0300

    wip - checklist dnd

--- src/cmps/TaskDetails/TaskDetailsChecklist.jsx
+++ src/cmps/TaskDetails/TaskDetailsChecklist.jsx
@@ -1,3 +1,4 @@
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd"
 import { Button } from "../Button"
 import SvgIcon from "../SvgIcon"
 import { TaskDetailsChecklistTodos } from "./TaskDetailsChecklistTodos"
@@ -10,31 +11,59 @@ export function TaskDetailsChecklist({
   onUpdateCheckListTodo,
 }) {
   return (
-    <section className="task-details-checklist">
-      {checklists?.map((checklist) => {
-        return (
-          <div className="task-details-checklist" key={checklist.id}>
-            {/* Checklist header */}
-            <div className="task-details-checklist-header task-detail-header-section">
-              <SvgIcon size={"md"} iconName={"checkbox"} />
-              <h3>{checklist.title}</h3>
-              <Button
-                variant="contained"
-                onClick={() => onRemoveChecklist(checklist.id)}
-              >
-                Delete
-              </Button>
-            </div>
-            <TaskDetailsChecklistTodos
-              checklistsId={checklist.id}
-              todos={checklist.todos}
-              onUpdateCheckListTodo={onUpdateCheckListTodo}
-              onAddCheckListTodo={onAddCheckListTodo}
-              onRemoveCheckListTodo={onRemoveCheckListTodo}
-            />
-          </div>
-        )
-      })}
-    </section>
+    <DragDropContext>
+      <Droppable
+        droppableId="checklist-section"
+        direction="vertical"
+        type="checklist-section"
+      >
+        {(provided) => (
+          <section
+            className="task-details-checklist"
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+          >
+            {checklists?.map((checklist, index) => {
+              return (
+                <Draggable
+                  key={checklist.id}
+                  draggableId={checklist.id}
+                  index={index}
+                >
+                  {(provided, snapshot) => (
+                    <div
+                      className="task-details-checklist"
+                      key={checklist.id}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      ref={provided.innerRef}
+                    >
+                      {/* Checklist header */}
+                      <div className="task-details-checklist-header task-detail-header-section">
+                        <SvgIcon size={"md"} iconName={"checkbox"} />
+                        <h3>{checklist.title}</h3>
+                        <Button
+                          variant="contained"
+                          onClick={() => onRemoveChecklist(checklist.id)}
+                        >
+                          Delete
+                        </Button>
+                      </div>
+                      <TaskDetailsChecklistTodos
+                        checklistsId={checklist.id}
+                        todos={checklist.todos}
+                        onUpdateCheckListTodo={onUpdateCheckListTodo}
+                        onAddCheckListTodo={onAddCheckListTodo}
+                        onRemoveCheckListTodo={onRemoveCheckListTodo}
+                      />
+                    </div>
+                  )}
+                </Draggable>
+              )
+            })}
+          </section>
+        )}
+      </Droppable>
+    </DragDropContext>
   )
 }
