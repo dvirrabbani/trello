a126c9bde9370c41b969c3b5ba88465966bff603
2xlyuval <2xlyuva@gmail.com>
2024-05-25 15:09:21 +0300

    wip - checklist section dragging - done

--- src/cmps/TaskDetails/TaskDetailsChecklist.jsx
+++ src/cmps/TaskDetails/TaskDetailsChecklist.jsx
@@ -2,6 +2,7 @@ import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd"
 import { Button } from "../Button"
 import SvgIcon from "../SvgIcon"
 import { TaskDetailsChecklistTodos } from "./TaskDetailsChecklistTodos"
+import { updateCurrentBoard } from "../../store/board.actions"
 
 export function TaskDetailsChecklist({
   checklists,
@@ -9,9 +10,44 @@ export function TaskDetailsChecklist({
   onAddCheckListTodo,
   onRemoveCheckListTodo,
   onUpdateCheckListTodo,
+  onUpdateTask,
 }) {
+  function onDragEnd(result) {
+    const { destination, source, draggableId, type } = result
+
+    // dropped outside the list
+    if (!destination) return
+    // dropped in the same place
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    )
+      return
+
+    if (type === "checklist-section") {
+      const newChecklists = handleDragInsideDroppable(
+        checklists,
+        source,
+        destination
+      )
+      onUpdateTask({
+        key: "checklists",
+        value: newChecklists,
+      })
+      console.log("newChecklists", newChecklists)
+      return
+    }
+  }
+
+  function handleDragInsideDroppable(items, source, destination) {
+    const newItems = Array.from(items)
+    const [reorderedItems] = newItems.splice(source.index, 1)
+    newItems.splice(destination.index, 0, reorderedItems)
+    return newItems
+  }
+
   return (
-    <DragDropContext>
+    <DragDropContext onDragEnd={onDragEnd}>
       <Droppable
         droppableId="checklist-section"
         direction="vertical"
@@ -32,14 +68,18 @@ export function TaskDetailsChecklist({
                 >
                   {(provided, snapshot) => (
                     <div
-                      className="task-details-checklist"
+                      className={`task-details-checklist ${
+                        snapshot.isDragging ? "dragging" : ""
+                      }`}
                       key={checklist.id}
                       {...provided.draggableProps}
-                      {...provided.dragHandleProps}
                       ref={provided.innerRef}
                     >
                       {/* Checklist header */}
-                      <div className="task-details-checklist-header task-detail-header-section">
+                      <div
+                        {...provided.dragHandleProps}
+                        className="task-details-checklist-header task-detail-header-section"
+                      >
                         <SvgIcon size={"md"} iconName={"checkbox"} />
                         <h3>{checklist.title}</h3>
                         <Button
@@ -61,6 +101,7 @@ export function TaskDetailsChecklist({
                 </Draggable>
               )
             })}
+            {provided.placeholder}
           </section>
         )}
       </Droppable>
