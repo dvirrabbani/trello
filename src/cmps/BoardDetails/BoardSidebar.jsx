86868ac72c6b9e902f330bf0f69706489e0867dc
2xlyuval <2xlyuval@gmail.com>
2024-06-02 23:50:42 +0300

    wip - board sidebar- towo component

--- src/cmps/BoardDetails/BoardSidebar.jsx
+++ src/cmps/BoardDetails/BoardSidebar.jsx
@@ -1,16 +1,18 @@
 import SvgIcon from "../SvgIcon"
+import { useState } from "react"
+import { useSelector } from "react-redux"
 import { updateBoard } from "../../store/board.actions"
 import { BoardNavLink } from "../BoardNavLink"
-import { useSelector } from "react-redux"
 import { AddBoardButton } from "../AddBoardButton"
 import { Button } from "../Button"
-import { useState } from "react"
+import { motion, AnimatePresence } from "framer-motion"
+import { Transition } from "react-transition-group"
 
 export function BoardSidebar() {
   const [sidebarCollapse, setSidebarCollapse] = useState(false)
   const boards = useSelector((storeState) => storeState.boardModule.boards)
   const user = useSelector((storeState) => storeState.userModule.user)
-  const userFirstletter = user.fullName.charAt(0).toUpperCase()
+  const userFirstLetter = user.fullName.charAt(0).toUpperCase()
 
   function onToggleBoardStarred(ev, board) {
     ev.preventDefault()
@@ -19,41 +21,41 @@ export function BoardSidebar() {
       value: !board.isStarred,
     })
   }
-  return sidebarCollapse ? (
-    <div className="collapse-sidebar">
-      <Button
-        className={"dynamic-button shape-circle"}
-        onClick={() => setSidebarCollapse(false)}
-      >
-        <SvgIcon iconName="arrow" />
-      </Button>
-    </div>
-  ) : (
-    <nav className="board-sidebar">
-      <header className="board-sidebar-header flex align-center justify-between">
-        <div className="user-workspace flex align-center">
-          <div className="user-thumbnail flex align-center justify-center">
-            <span className="user-thumbnail-letter">{userFirstletter}</span>
-          </div>
-          <div className="user-info">
-            <div className="user-fullname">{user.fullName}</div>
-            <div className="plan-type">Free</div>
-          </div>
-        </div>
+
+  return (
+    <>
+      <div className="collapse-sidebar">
         <Button
-          className={"dynamic-button"}
-          onClick={() => setSidebarCollapse(true)}
+          className="dynamic-button shape-circle"
+          onClick={() => setSidebarCollapse(false)}
         >
           <SvgIcon iconName="arrow" />
         </Button>
-      </header>
-      <ul className="board-sidebar-list clean-list flex column">
-        <div className="board-sidebar-list-header">
-          <span className="title">Your boards</span>
-          <AddBoardButton iconName={"plus"} />
-        </div>
-        {boards?.map((board) => {
-          return (
+      </div>
+      <nav className="board-sidebar">
+        <header className="board-sidebar-header flex align-center justify-between">
+          <div className="user-workspace flex align-center">
+            <div className="user-thumbnail flex align-center justify-center">
+              <span className="user-thumbnail-letter">{userFirstLetter}</span>
+            </div>
+            <div className="user-info">
+              <div className="user-fullname">{user.fullName}</div>
+              <div className="plan-type">Free</div>
+            </div>
+          </div>
+          <Button
+            className="dynamic-button"
+            onClick={() => setSidebarCollapse(true)}
+          >
+            <SvgIcon iconName="arrow" />
+          </Button>
+        </header>
+        <ul className="board-sidebar-list clean-list flex column">
+          <div className="board-sidebar-list-header">
+            <span className="title">Your boards</span>
+            <AddBoardButton iconName="plus" />
+          </div>
+          {boards?.map((board) => (
             <li className="board-sidebar-item" key={board._id}>
               <BoardNavLink board={board} />
               <SvgIcon
@@ -61,9 +63,9 @@ export function BoardSidebar() {
                 iconName={board.isStarred ? "starFill" : "star"}
               />
             </li>
-          )
-        })}
-      </ul>
-    </nav>
+          ))}
+        </ul>
+      </nav>
+    </>
   )
 }
