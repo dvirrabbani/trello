2df633c307e96dd442caac340fc524fbd99574ab
Dvir <rabbanidvir@gmail.com>
2024-06-05 16:47:50 +0300

    wip - login demo user

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,30 @@
 import { useEffect, useRef } from "react"
 import { Button } from "../cmps/Button"
 import SvgIcon from "../cmps/SvgIcon"
+import { userService } from "../services/user.service"
+import { useForm } from "../customHooks/useForm"
+import { useNavigate } from "react-router"
 
 export function Login() {
+  const navigate = useNavigate()
+  const [fields, , handleChange, resetForm] = useForm({
+    email: "taltarablus@gmail.com",
+    password: "Tal Tarablus",
+  })
   const emailRef = useRef(null)
 
   useEffect(() => {
     emailRef.current.focus()
+    return () => resetForm()
   }, [])
 
+  async function onLogin(ev) {
+    ev.preventDefault()
+    const user = await userService.login(fields)
+    if (user._id) {
+      navigate("/board")
+    }
+  }
   return (
     <div className="login">
       <section className="login-card">
@@ -16,13 +32,17 @@ export function Login() {
           <SvgIcon iconName={"loginLogo"} size={"full"} />
           <h3>Log in to continue</h3>
         </header>
-        <form>
+        <form onSubmit={onLogin}>
           <input
-            ref={emailRef}
+            name="email"
             type="email"
             className="input-text"
             placeholder="Enter your email"
+            ref={emailRef}
+            value={fields.email}
+            onChange={handleChange}
           />
+          <input name="password" type="password" className="input-text" value={fields.password} onChange={handleChange} />
           <Button className={"w-100 flex justify-center"} variant="primary">
             Continue
           </Button>
