2c5290350d521c9a93cf7b71d113f9c4b4434340
2xlyuval <2xlyuval@gmail.com>
2024-06-09 23:05:52 +0300

    BoardMenu - custom bg

--- src/cmps/BoardDetails/DynamicBoardMenu/ChangeBoardBg.jsx
+++ src/cmps/BoardDetails/DynamicBoardMenu/ChangeBoardBg.jsx
@@ -1,15 +1,28 @@
 import { uiService } from "../../../services/ui.service"
+import { updateCurrentBoard } from "../../../store/board.actions"
+import { BtnImgUploader } from "../../btn/BtnImgUpload"
 
 export function ChangeBoardBg({ type, setMenuContent }) {
   const colorsBgUrl =
     "https://res.cloudinary.com/df0eaacho/image/upload/v1717878971/97db30fe74a58b7b7a18_ah8ljq.png"
   const imgBgUrl =
     "https://res.cloudinary.com/df0eaacho/image/upload/v1717878972/8f9c1323c9c16601a9a4_y4kqes.jpg"
-  const bgOptionsUrl =
+  const bgOptions =
     type === "bg-color" ? uiService.getBoardBgSvg() : uiService.getBoardBgImg()
 
   function onClickBgGroup(title, type) {
     setMenuContent({ title, type })
+  }
+
+  function onClickBg(bgOption) {
+    updateCurrentBoard(null, null, { key: "style", value: bgOption })
+  }
+
+  async function onUpdateCustomBg(bgImg) {
+    const colorRgb = await uiService.getDominantColor(bgImg)
+    const themeColor = uiService.isRgbBright(colorRgb) ? "dark" : "light"
+    const boardStyleToUpdate = { bgImg, colorRgb, themeColor }
+    updateCurrentBoard(null, null, { key: "style", value: boardStyleToUpdate })
   }
 
   return type === "background" ? (
@@ -28,15 +41,18 @@ export function ChangeBoardBg({ type, setMenuContent }) {
       </div>
       <div className="divider"></div>
       <h3>Custom background</h3>
-      <div>+</div>
+      <div>
+        <BtnImgUploader title={"+"} onUploaded={onUpdateCustomBg} />
+      </div>
     </div>
   ) : (
     <div className="bg-option-list">
-      {bgOptionsUrl.map((bgOptionUrl, index) => (
+      {bgOptions.map((bgOption, index) => (
         <BgOption
           key={index}
-          imgUrl={bgOptionUrl.bgImg}
-          emoji={bgOptionUrl.emoji}
+          imgUrl={bgOption.bgImg}
+          emoji={bgOption.emoji}
+          onClick={() => onClickBg(bgOption)}
         />
       ))}
     </div>
