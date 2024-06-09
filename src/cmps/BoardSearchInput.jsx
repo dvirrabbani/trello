fd546a73b7b0b50f7c942acdecead0b8493d83a6
Dvir <rabbanidvir@gmail.com>
2024-06-09 15:23:37 +0300

    fix - close search option when clicking outside

--- src/cmps/BoardSearchInput.jsx
+++ src/cmps/BoardSearchInput.jsx
@@ -1,14 +1,15 @@
-import { useSelector } from "react-redux"
-import SvgIcon from "./SvgIcon"
-import { useState } from "react"
-import { Button } from "./Button"
+import { useRef, useState } from "react"
 import { useNavigate, useParams } from "react-router"
+import { useSelector } from "react-redux"
+import { useClickedOutside } from "../customHooks/useClickOutside"
+import SvgIcon from "./SvgIcon"
+import { Button } from "./Button"
 
 export function BoardSearchInput() {
   const navigate = useNavigate()
   const params = useParams()
   const boards = useSelector((storeState) => storeState.boardModule.boards)
-
+  const searchRef = useRef()
   const [searchBy, setSearchBy] = useState("")
   const [isOptionsOpen, setIsOptionsOpen] = useState(false)
   const filterBoardByText = boards?.filter((board) => board.title.toLowerCase().includes(searchBy.toLowerCase()))
@@ -18,6 +19,12 @@ export function BoardSearchInput() {
     setIsOptionsOpen(false)
     setSearchBy("")
   }
+
+  function onClose() {
+    setIsOptionsOpen(false)
+  }
+
+  useClickedOutside(searchRef, onClose)
 
   return (
     <section className={`board-search-input${Object.keys(params).length === 0 ? " board-index-search" : ""}`}>
@@ -34,7 +41,7 @@ export function BoardSearchInput() {
 
       {/* Filter board by search by text */}
       {isOptionsOpen && (
-        <section className="search-options">
+        <section className="search-options" ref={searchRef}>
           <span className="search-title">Recent boards</span>
           {filterBoardByText?.map((board) => (
             <Button key={board._id} className="search-option" onClick={() => onSelectBoard(board._id)}>
