8444d66cdc7980f86be2fd321903671488fd58bc
Dvir <rabbanidvir@gmail.com>
2024-06-10 18:47:08 +0300

    wip - star mini board

--- src/cmps/BoardList.jsx
+++ src/cmps/BoardList.jsx
@@ -1,9 +1,12 @@
+import { boardService } from "../services/board.service"
 import { updateBoard } from "../store/board.actions"
 import { BoardPreview } from "./boardPreview"
 import SvgIcon from "./SvgIcon"
 
 export function BoardList({ boards, children }) {
-  async function onUpdateBoard(board, { key, value }) {
+  async function onUpdateBoard(boardId, { key, value }) {
+    const board = await boardService.getById(boardId)
+
     updateBoard(board, { key, value })
   }
 
@@ -14,16 +17,16 @@ export function BoardList({ boards, children }) {
         return (
           <li className="board-item" key={board._id}>
             <BoardPreview id={_id} title={title} style={style} />
-            <div className="board-actions">
-              <button
-                className="clean-btn "
-                onClick={() =>
-                  onUpdateBoard(board, {
-                    key: "isStarred",
-                    value: !board.isStarred,
-                  })
-                }
-              >
+            <div
+              className={`board-actions${board.isStarred ? " starred-board" : " unstarred-board"}`}
+              onClick={() =>
+                onUpdateBoard(board._id, {
+                  key: "isStarred",
+                  value: !board.isStarred,
+                })
+              }
+            >
+              <button className="clean-btn">
                 <SvgIcon iconName={board.isStarred ? "starFill" : "star"} />
               </button>
             </div>
