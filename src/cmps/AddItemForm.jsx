2fe09283b1ef6f957fa278eb1495cd6928f5058c
2xlyuval <2xlyuval@gmail.com>
2024-06-12 15:53:12 +0300

    AddItemForm - ClickAwayListener

--- src/cmps/AddItemForm.jsx
+++ src/cmps/AddItemForm.jsx
@@ -1,5 +1,6 @@
 import { forwardRef, useEffect, useState } from "react"
 import SvgIcon from "./SvgIcon"
+import { ClickAwayListener } from "@mui/material"
 
 export const AddItemForm = forwardRef(
   ({ onAddItem, setDisplayAddItem, type }, ref) => {
@@ -10,42 +11,49 @@ export const AddItemForm = forwardRef(
       setInputVal(value)
     }
 
+    function handleClickAway() {
+      setDisplayAddItem(false)
+      setInputVal("")
+    }
+
     const placeholder =
       type === "task" ? "Enter a title for this card..." : "Enter list title..."
 
     return (
-      <div className="add-item-form" ref={ref}>
-        <form className={`${type == "group" ? "group-preview" : ""}`}>
-          <div className={`flex ${type == "task" ? "task-preview" : ""}`}>
-            <textarea
-              name="title"
-              spellCheck="false"
-              value={inputVal}
-              onChange={handleChange}
-              placeholder={placeholder}
-              onKeyDown={(e) => {
-                if (e.key === "Enter") onAddItem(e, inputVal, setInputVal)
-              }}
-              autoFocus
-            ></textarea>
-          </div>
-          <div className="add-item-actions flex">
-            <button
-              className="button variant-primary"
-              onClick={(e) => onAddItem(e, inputVal)}
-              type="submit"
-            >
-              {`Add ${type == "task" ? "card" : "list"}`}
-            </button>
-            <button
-              className="button variant-text shape-regular"
-              onClick={() => setDisplayAddItem(false)}
-            >
-              <SvgIcon iconName="close" />
-            </button>
-          </div>
-        </form>
-      </div>
+      <ClickAwayListener onClickAway={handleClickAway}>
+        <div className="add-item-form" ref={ref}>
+          <form className={`${type == "group" ? "group-preview" : ""}`}>
+            <div className={`flex ${type == "task" ? "task-preview" : ""}`}>
+              <textarea
+                name="title"
+                spellCheck="false"
+                value={inputVal}
+                onChange={handleChange}
+                placeholder={placeholder}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") onAddItem(e, inputVal, setInputVal)
+                }}
+                autoFocus
+              ></textarea>
+            </div>
+            <div className="add-item-actions flex">
+              <button
+                className="button variant-primary"
+                onClick={(e) => onAddItem(e, inputVal)}
+                type="submit"
+              >
+                {`Add ${type == "task" ? "card" : "list"}`}
+              </button>
+              <button
+                className="button variant-text shape-regular"
+                onClick={() => setDisplayAddItem(false)}
+              >
+                <SvgIcon iconName="close" />
+              </button>
+            </div>
+          </form>
+        </div>
+      </ClickAwayListener>
     )
   }
 )
