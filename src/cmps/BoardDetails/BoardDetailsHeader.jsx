4b4fc5eb6cc5f4af560426a1cd6642f45d413d85
Dvir <rabbanidvir@gmail.com>
2024-06-15 14:53:57 +0300

    wip - share board

--- src/cmps/BoardDetails/BoardDetailsHeader.jsx
+++ src/cmps/BoardDetails/BoardDetailsHeader.jsx
@@ -11,14 +11,9 @@ import { boardService } from "../../services/board.service"
 import { ProfileImg } from "../ProfileImg"
 import { useForm } from "../../customHooks/useForm"
 import { set } from "date-fns"
+import { ShareBoardButton } from "../ShareBoardButton"
 
-export function BoardDetailsHeader({
-  board,
-  filterBy,
-  viewType,
-  setViewType,
-  onToggleBoardMenu,
-}) {
+export function BoardDetailsHeader({ board, filterBy, viewType, setViewType, onToggleBoardMenu }) {
   const [anchorEl, setAnchorEl] = useState(null)
   const [fields, setFields, handleChange, resetForm] = useForm({
     title: board.title,
@@ -99,11 +94,7 @@ export function BoardDetailsHeader({
     <section className="board-details-header">
       <div className="board-topbar-start">
         <div className="board-title-container">
-          <h1
-            className="board-details-title"
-            onClick={handleTitleClick}
-            ref={boardTitleH1}
-          >
+          <h1 className="board-details-title" onClick={handleTitleClick} ref={boardTitleH1}>
             {board.title}
           </h1>
           <input
@@ -119,24 +110,17 @@ export function BoardDetailsHeader({
           />
         </div>
         <Button>
-          <SvgIcon
-            onClick={onToggleBoardStarred}
-            iconName={board.isStarred ? "starFill" : "star"}
-          />
+          <SvgIcon onClick={onToggleBoardStarred} iconName={board.isStarred ? "starFill" : "star"} />
         </Button>
         <Button
-          className={`board-details-header-button dynamic-button${
-            viewType === "board" ? " active" : ""
-          }`}
+          className={`board-details-header-button dynamic-button${viewType === "board" ? " active" : ""}`}
           onClick={() => onChangeViewType("board")}
         >
           <SvgIcon iconName="boardView" />
           <span>Board</span>
         </Button>
         <Button
-          className={`board-details-header-button dynamic-button${
-            viewType === "dashboard" ? " active" : ""
-          }`}
+          className={`board-details-header-button dynamic-button${viewType === "dashboard" ? " active" : ""}`}
           onClick={() => onChangeViewType("dashboard")}
         >
           <SvgIcon iconName="dashboard" />
@@ -144,15 +128,8 @@ export function BoardDetailsHeader({
         </Button>
       </div>
       <div className="board-topbar-end">
-        <span
-          className={`filter-btns-container flex ${
-            !isFilterEmpty || isPopoverOpen ? "active" : ""
-          }`}
-        >
-          <button
-            className="filter-btn button dynamic-button"
-            onClick={handleClick}
-          >
+        <span className={`filter-btns-container flex ${!isFilterEmpty || isPopoverOpen ? "active" : ""}`}>
+          <button className="filter-btn button dynamic-button" onClick={handleClick}>
             <SvgIcon iconName="filter" />
             Filters
             {!isFilterEmpty && <FilterCount tasksCount={tasksCount} />}
@@ -170,27 +147,19 @@ export function BoardDetailsHeader({
           onClose={handleClose}
           title="Filter"
         >
-          <BoardFilter
-            members={board.members}
-            labels={board.labels}
-            filterBy={filterBy}
-          />
+          <BoardFilter members={board.members} labels={board.labels} filterBy={filterBy} />
         </Popover>
         <div className="divider"></div>
         <ul className="member-list">
           {board.members.map((member) => {
             return (
-              <li
-                key={member.id}
-                className="member-item"
-                draggable="true"
-                onDragStart={(e) => handleMemberDragStart(e, member)}
-              >
+              <li key={member.id} className="member-item" draggable="true" onDragStart={(e) => handleMemberDragStart(e, member)}>
                 <ProfileImg member={member} size={"md"} />
               </li>
             )
           })}
         </ul>
+        <ShareBoardButton />
         <Button className={"dynamic-button"} onClick={onToggleBoardMenu}>
           <SvgIcon iconName="more" />
         </Button>
---
+++ src/cmps/ShareBoardButton.jsx
@@ -0,0 +1,76 @@
+import { useState } from "react"
+import { useSelector } from "react-redux"
+import { Popover } from "./Popover"
+import { Button } from "./Button"
+import SvgIcon from "./SvgIcon"
+import { ProfileImg } from "./ProfileImg"
+import { userService } from "../services/user.service"
+import { updateBoard } from "../store/board.actions"
+
+export function ShareBoardButton() {
+  const board = useSelector((storeState) => storeState.boardModule.board)
+  const [users, setUsers] = useState(null)
+  const [anchorEl, setAnchorEl] = useState(null)
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
+  async function onShareBoard(event) {
+    const target = event.currentTarget
+    const users = await userService.getAppUsers()
+    setUsers(users)
+    setAnchorEl(target)
+  }
+
+  function onShareMemberToBoard(member) {
+    let boardMembersToSave = [...board.members]
+    const memberToSave = { ...member }
+    memberToSave.id = memberToSave._id
+    delete memberToSave._id
+    const mIdx = boardMembersToSave.findIndex((boardMember) => boardMember.id === memberToSave.id)
+    if (mIdx < 0) {
+      boardMembersToSave.push(memberToSave)
+      // must have at least one member
+    } else if (boardMembersToSave.length > 1) {
+      boardMembersToSave.splice(mIdx, 1)
+    }
+    updateBoard(board, { key: "members", value: boardMembersToSave })
+    setAnchorEl(null)
+  }
+
+  const isPopoverOpen = Boolean(anchorEl) && users
+
+  return (
+    <div className="share-board-button">
+      <Button className="share-button" onClick={onShareBoard}>
+        <SvgIcon iconName="shareUser" />
+        <span>Share</span>
+      </Button>
+      {isPopoverOpen && (
+        <Popover
+          id={isPopoverOpen ? "app-members-id" : undefined}
+          open={isPopoverOpen}
+          anchorEl={anchorEl}
+          onClose={handleClose}
+          title={"Members"}
+        >
+          <div className="app-members ">
+            {users.length > 0 && (
+              <ul className="clean-list flex column">
+                {users.map((member) => {
+                  return (
+                    <Button key={`shared-board-${member._id}`} onClick={() => onShareMemberToBoard(member)}>
+                      <ProfileImg member={member} size={"lg"} />
+                      <span>{member.fullName}</span>
+                    </Button>
+                  )
+                })}
+              </ul>
+            )}
+          </div>
+        </Popover>
+      )}
+    </div>
+  )
+}
