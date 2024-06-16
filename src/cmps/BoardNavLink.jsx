0c8114f0ced861b694832e4e1e77d7974ed4be45
2xlyuval <2xlyuval@gmail.com>
2024-06-16 21:14:30 +0300

    fix - when navigate sidebar: clear filter

--- src/cmps/BoardNavLink.jsx
+++ src/cmps/BoardNavLink.jsx
@@ -1,10 +1,26 @@
-import { NavLink } from "react-router-dom"
+import { NavLink, useNavigate, useParams } from "react-router-dom"
+import { store } from "../store/store"
+import { boardService } from "../services/board.service"
 
 export function BoardNavLink({ board }) {
+  const navigate = useNavigate()
+  const { boardId } = useParams()
+
+  function goToBoard() {
+    store.dispatch({
+      type: "SET_BOARD_FILTER",
+      filterBy: boardService.getDefaultFilter(),
+    })
+    navigate(`/board/${board._id}`)
+  }
+
   return (
-    <NavLink className={"board-nav-link"} to={`/board/${board._id}`}>
+    <div
+      className={`board-nav-link ${boardId == board._id ? "active" : ""}`}
+      onClick={goToBoard}
+    >
       <img className="thumbnail" src={board.style.bgImg} alt="board image" />
       <span>{board.title}</span>
-    </NavLink>
+    </div>
   )
 }
